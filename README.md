8f7267e908aa801292a681b9ab009edf861de16b
Finkch <skyler@thealdersons.org>
2024-07-02 11:51:32 -0700

    Added dump command

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # finkchutil
 
 A handful of system utility functions for the fantasy workstation Picotron. These functions focus on integrating external editors into the Picotron workflow.
+
+
+## dump
+
+This command dumps the current cartridge in RAM to the specified folder on the desktop. If no folder is specified, it creates a folder called "dump".  
+Warning: this command deletes preexisting contents of the specified folder.
---
+++ src/dump.lua
@@ -0,0 +1,49 @@
+--[[
+	
+	Dumps the current cartridge's contents to storage.
+	
+	Useful for developing using external editor.
+
+]]
+
+cd(env().path)
+
+local argv = env().argv
+
+-- Acquires target directory
+local path = nil
+if (not argv[1]) then
+	print("no directory specified, writing to temporary folder")
+	path = "desktop/temp"
+else
+	path = "desktop/" .. argv[1]
+end
+
+-- Clears directory if it exists
+if (fstat(path) == "folder") or (fstat(path) == "file") then
+	err = rm(path)
+	if (err) then
+		print(err)
+		exit(0)
+	end
+end
+
+-- Creates directory at the path
+err = mkdir(path)
+if (err) then
+	print(err)
+	exit(0)
+end
+
+-- Gets a list of all files to be dumped
+local res = ls("ram/cart")
+if (not res) then
+	print("cartridge not found")
+	exit(0)
+end
+
+-- Copies all files to the dump folder
+for i = 1, #res do
+	local err = cp("ram/cart/" .. res[i], path .. "/" .. res[i])
+	if (err) print(err)
+end	
